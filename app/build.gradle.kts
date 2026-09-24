311ab8e95d5be529892864e4fd000f9e092175f0
steyyrddsf <youmatisagnon@gmail.com>
2026-09-24 06:53:57 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.example.usagetracker"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.usagetracker"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
